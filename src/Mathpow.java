2c68bb46d131e301d62179d9d4fd7b5df10c098b
Ashwin <ashwinnanbazhagan@gmail.com>
2023-08-24 21:33:32 +0530

    DSA

---
+++ src/Mathpow.java
@@ -0,0 +1,18 @@
+public class Mathpow {
+    public static void main(String[] args) {
+        double x =2;
+        int n =-2;
+        System.out.println(Math.pow(x,n));
+        System.out.println(myPow(x,n));
+
+    }
+    public static double myPow(double x, int n) {
+        double product = 1;
+        return solution(product,x,n);
+    }
+    public static double solution(double product , double x , int n){
+        if(n==1) return x;
+        product = x * solution(product,x,n-1);
+        return product;
+    }
+}
