367fce4e683cb94fe3a1f27d428efeede4739740
Ashwin <ashwinnanbazhagan@gmail.com>
2023-09-06 18:50:38 +0530

    DSA

--- src/TargetSubset.java
+++ src/TargetSubset.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-
 public class TargetSubset {
     public static void main(String[] args) {
         int[] arr = {1,2,3};
@@ -27,7 +26,7 @@ public class TargetSubset {
         dp[i][target]=taken + not;
         return dp[i][target];
     }
-    public static int findWays(int num[], int tar) {
+    public static int findWays(int[] num, int tar) {
         int n = num.length;
         int[][] dp = new int[n][tar+1];
         for ( int i =0;i<n;i++){
